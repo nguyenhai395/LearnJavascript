1dbe9eb29adf032b61c803d999d90aec0ad59736
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-08 15:58:26 +0900

    16h00

--- LearnJvs/Test1.js
+++ LearnJvs/Test1.js
@@ -1,4 +1,16 @@
-let a= 5;
-let b=10;
-console.log(` Hello Word with HarryNguyen `)
-console.log(` So twenty la ${a*4} not ${a+b}`)
+// let a= 5;
+// let b=10;
+// console.log(` Hello Word with HarryNguyen `)
+// console.log(` So twenty la ${a*4} not ${a+b}`)
+// Phan nay la phan Template. viet ngan gon trong code
+
+//Object : Muc 8
+
+// key : value, key2 : value2
+console.log(` Hello with learn Part 8`)
+let a= {
+    name: `harryNguyen`,
+    address:`Bac Giang`
+}
+let b = `Harry`
+console.log(` Type of a : `, typeof a,`type of b : `, typeof b);
