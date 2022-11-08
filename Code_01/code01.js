b0d661a0acb3a02965498813eba17d8fa441e5cc
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-08 10:48:30 +0900

    update 11h00

--- Code_01/code01.js
+++ Code_01/code01.js
@@ -1,2 +1,6 @@
-    // alert(" you are lucky!!")
-    console.log("Hello word")
+console.log(" hello world, for begin JavaScript")
+let a = 1;
+let b= 2.1
+let c = "xinchao";
+console.log(" Cac gia tri se la ","a =",a ,"b= ",b,"c =", c)
+console.log("Gia tri true false se la ",a===b)
