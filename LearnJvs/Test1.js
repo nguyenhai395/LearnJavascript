15b3bc7f1e3416889282cb41d89a64974e00f53a
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-08 13:13:39 +0900

    update 13h13

--- LearnJvs/Test1.js
+++ LearnJvs/Test1.js
@@ -1,9 +1,10 @@
-alert(" WellCome to Page ")
-console.log(" HelloWorld")
+//alert(" WellCome to Page ")
+console.log(" HelloWorld".toLocaleUpperCase());
 let a=1;
 let b=2.1;
 let c="ABCXYZ"
 console.log("Gia tri cua a la ",a)
 console.log("Gia tri cua b la ",b)
-console.log("Chuoi string la ", c)
+console.log("Chuoi string la ", c.toLocaleLowerCase())
 console.log("Do dai cua chuoi ",c.length)
+console.log("Gia tri cua a+b la ", a+b);
