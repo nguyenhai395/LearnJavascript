fa2c44e487136b0b34d0a27cc03c81f8a4261a07
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-08 11:14:25 +0900

    update 11h14m

--- Code_01/code01.js
+++ Code_01/code01.js
@@ -1,6 +1,8 @@
 console.log(" hello world, for begin JavaScript")
-let a = 1;
-let b= 2.1
-let c = "xinchao";
-console.log(" Cac gia tri se la ","a =",a ,"b= ",b,"c =", c)
-console.log("Gia tri true false se la ",a===b)
+// let a = 1;
+// let b= 2.1
+// let c = "xinchao";
+// console.log(" Cac gia tri se la ","a =",a ,"b= ",b,"c =", c)
+// console.log("Gia tri true false se la ",a===b)
+let a= " Toi la String - chuoi "
+console.log(a);
