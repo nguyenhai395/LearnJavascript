f3818a863f6d5606b0e9c432543df996eab8b648
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-08 11:55:27 +0900

    Update 11h55

---
+++ LearnJvs/Test1.js
@@ -0,0 +1,9 @@
+alert(" WellCome to Page ")
+console.log(" HelloWorld")
+let a=1;
+let b=2.1;
+let c="ABCXYZ"
+console.log("Gia tri cua a la ",a)
+console.log("Gia tri cua b la ",b)
+console.log("Chuoi string la ", c)
+console.log("Do dai cua chuoi ",c.length)
