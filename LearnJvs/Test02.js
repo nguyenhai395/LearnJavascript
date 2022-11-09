0f7539a094d93663aa0946b5ac228365d7297c1e
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-09 14:01:18 +0900

    update 221109 / 14h01

---
+++ LearnJvs/Test02.js
@@ -0,0 +1,17 @@
+console.log(`Well Come to training with JVScript `)
+console.log(`For Lesson 8`)
+let a= 1
+let b=2.2
+let c=3
+console.log(` For training part 7`)
+console.log(` Seven is ${a+(c*2)} is not ${a+b}`)
+console.log(`--------------------`)
+let inf={
+    name: "Hary",
+    address: "VietNam",
+    email : "nguyenhhai86@gmail.com"
+}
+
+console.log(`My name is `, inf.name)
+console.log(`My address is `,inf.address)
+console.log(`My email is `, inf.email)
