6d2696413a389e9a77218ab5c55e8c0486a738de
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-10 14:47:53 +0900

    update by Hai

--- LearnJvs/Sesean3.js
+++ LearnJvs/Sesean3.js
@@ -1,30 +1,36 @@
-// alert(" You are Lucky!!")
-console.log(" Hello World")
-console.log("To day, we training in Object Part 7")
-let info={
-    name: `Harry Nguyen`,
-    address: `Viet Nam`,
-    email:`nguyenhai@gmail.com`
-}
-console.log("My name is ", info.name)
-console.log("My address is ",info.address)
-console.log(" End, my email is ", info.email)
+// // alert(" You are Lucky!!")
+// console.log(" Hello World")
+// console.log("To day, we training in Object Part 7")
+// let info={
+//     name: `Harry Nguyen`,
+//     address: `Viet Nam`,
+//     email:`nguyenhai@gmail.com`
+// }
+// console.log("My name is ", info.name)
+// console.log("My address is ",info.address)
+// console.log(" End, my email is ", info.email)
 
-console.log("Tobe continue, to learn For ")
-for(let i=0; i<10; i++){
-console.log("Gia tri cua i la ",i)
-}
+// console.log("Tobe continue, to learn For ")
+// for(let i=0; i<10; i++){
+// console.log("Gia tri cua i la ",i)
+// }
 
-let x=[1, 2, 3, 4, 5, 6]
-console.log(" Gia tri cua mang X[1]", x[2])
+// let x=[1, 2, 3, 4, 5, 6]
+// console.log(" Gia tri cua mang X[1]", x[2])
 
-console.log("To learn While do")
-let y=1;
-let flash = false
-while(!flash){
-    console.log("Gia tri cua y ", y)
-    y++;
-    if(y===10) break;
-}
+// console.log("To learn While do")
+// let y=1;
+// let flash = false
+// while(!flash){
+//     console.log("Gia tri cua y ", y)
+//     y++;
+//     if(y===10) break;
+// }
 
 // OK. den day la hoc xong phan While
+
+console.log(" To learn Function")
+function sum( a,b ){
+    return a+b;
+}
+console.log(" Tinh tong hai so 2 va 5 se la ", sum(2,5))
