3408e40fb2549137e9bdbab48785c10b485900cb
=HarryNguyen <nguyenhhai86@gmail.com>
2022-11-10 14:02:51 +0900

    update 221110 by Hai

---
+++ LearnJvs/Sesean3.js
@@ -0,0 +1,19 @@
+// alert(" You are Lucky!!")
+console.log(" Hello World")
+console.log("To day, we training in Object Part 7")
+let info={
+    name: `Harry Nguyen`,
+    address: `Viet Nam`,
+    email:`nguyenhai@gmail.com`
+}
+console.log("My name is ", info.name)
+console.log("My address is ",info.address)
+console.log(" End, my email is ", info.email)
+
+console.log("Tobe continue, to learn For ")
+for(let i=0; i<10; i++){
+console.log("Gia tri cua i la ",i)
+}
+
+let x=[1, 2, 3, 4, 5, 6]
+console.log(" Gia tri cua mang X[1]", x[2])
